a12196d3039dbb84f2b7f83355a525611969499f
bienestarsonora <depsbienestar@gmail.com>
2026-08-18 10:22:32 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@
 // siempre que Row Level Security (RLS) esté correctamente configurado.
 window.COMITES_SUPABASE = {
   url: 'https://kftmqgxiehfrsdpxckan.supabase.co',
-  key: 'sb_publishable_zgR5j_VXtp56K2VEG_IvOw_QcE8_hmS'
+  key: 'zgR5j_VXtp56K2VEG_IvOw_QcE8_hmS'
 };
